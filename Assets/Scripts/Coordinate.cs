b79b5799920ae922d5cd492c1b0dafa2e555d687
nicholasmirchandani <nmirchandani@chapman.edu>
2020-09-28 12:03:20 -0700

    Second setp to adding a gitignore

---
+++ Assets/Scripts/Coordinate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Created by Sean Robbins
+/// Date: 9/16/2020
+/// 
+/// This is the class that creates the objects used to store the locations of the player controlled tiles
+/// 
+/// </summary>
+public class Coordinate : MonoBehaviour
+{
+    public int row;
+
+    public int col;
+
+    public int startRow;
+
+    public int startCol;
+
+    public bool isCenter;
+
+    public Tile tile;
+
+    public SpriteRenderer spriteRenderer;
+
+    public bool hasMoved;
+
+    public bool canNotMove;
+
+    [Tooltip("The desired end angle of its next rotation, used to determine how it will rotate")]
+    public int rotateState;
+
+    public void Start()
+    {
+        startRow = row;
+        startCol = col;
+    }
+
+    public void MoveDown()
+    {
+        row -= 1;
+    }
+
+    public void MoveLeft()
+    {
+        col -= 1;
+    }
+
+    public void MoveRight()
+    {
+        col += 1;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Tooltip("Determines if player can keep playing or not")]
+    public bool death;
+
+    public static GameManager Instance;
+
+    [Tooltip("a float representing player input for movement, If negative move left, if positive move right")]
+    public float horizontalMovement;
+
+    public bool rotate;
+    public bool PlayerTurn;
+
+    public Color PlayerControlledColor;
+    public Color StableColor;
+    public Color BackgroundColor;
+
+    public float prevHorizontalMovement;
+
+    [SerializeField]
+    public List<GameObject> prefabShapesToGenerate;
+
+    public List<Color> ShapeColors;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.Instance == null)
+        {
+            GameManager.Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        if(prefabShapesToGenerate.Count == 0)
+        {
+            Debug.Log("There are no shapes to generate");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        prevHorizontalMovement = horizontalMovement;
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        rotate = Input.GetButtonDown("Jump");
+        if(Input.GetButtonDown("Cancel"))
+        {
+            Quit();
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public GameObject GetShapeToGenerate()
+    {
+        int numShapes = prefabShapesToGenerate.Count;
+        float randValue = Random.value;
+        float index = randValue;
+        for (int i = 1; i <= numShapes; ++i)
+        {
+            if (index <= i / (float)numShapes)
+            {
+                index = i-1;
+                break;
+            }
+        }
+
+        int numColors = ShapeColors.Count;
+        float colorIndex = randValue;
+        for (int i = 1; i <= numColors; ++i)
+        {
+            if (colorIndex <= i / (float)numColors)
+            {
+                colorIndex = i - 1;
+                break;
+            }
+        }
+
+        PlayerControlledColor = ShapeColors[(int)colorIndex];
+        return prefabShapesToGenerate[(int)index]; 
+    }
+}
